019996cd286a79446d892808ff1624b19ae52fd5
SumeyraAltinisik <sfa2023@gmail.com>
2022-12-27 19:52:36 +0300

    Add project files.

---
+++ MyDataService/Controllers/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyDataService.Models;
+
+namespace MyDataService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StoreController : Controller
+    {
+        [HttpGet(Name = "GetAlbums")]
+        public IEnumerable<Album> Index()
+        {
+
+            ChinookContext cnt = new ChinookContext();
+            return cnt.Albums;
+
+            
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+using MyDataService.Models;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async  Task<IActionResult> Index()
+        {
+            List<Gubudik> Listem = new List<Gubudik>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5205/WeatherForecast"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    Listem = JsonConvert.DeserializeObject<List<Gubudik>>(apiResponse);
+                }
+            }
+
+            return View(Listem);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
